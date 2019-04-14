43541c67bbd0aff2b48a2a5bc8bb0bebb60ccc7b
sadness_ojisan <sadness.ojisan@gmail.com>
2019-04-14 19:02:47 +0900

    add: button component

---
+++ src/component/common/Button.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import styled from "styled-components";
+
+interface IProps {
+  className: string;
+  onClick: any;
+  children: React.Node;
+}
+
+const Button = (props: IProps) => {
+  const { children, className } = props;
+  return <StyledButton className={className}>{children}</StyledButton>;
+};
+
+const StyledButton = styled.button<IProps>``;
+
+export { Button };
